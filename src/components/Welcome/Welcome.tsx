c570e1df532e14b0aae554df519385e304fae06e
alima987 <alima1996b@gmail.com>
2023-12-13 21:40:09 +0500

    feat: add text

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -9,11 +9,31 @@ export const Welcome = () => {
       </header>
       <section className="about">
         <h2>About the Project</h2>
-        <p>Here goes some information about the project...</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+          incidunt officia recusandae accusamus ipsam. Eius nam vitae eveniet
+          debitis eligendi assumenda explicabo voluptas illo ducimus. Dicta
+          atque voluptatibus doloremque nam quas quia dolores aperiam harum quos
+          consectetur perferendis error obcaecati minus, optio neque sequi? Unde
+          provident debitis aperiam facere eaque.
+        </p>
         <h2>About the Developers</h2>
-        <p>Meet our talented team of developers...</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium
+          nostrum voluptatibus tenetur nam vitae totam aperiam eveniet et odit
+          harum veritatis, culpa sint omnis esse inventore, optio
+          necessitatibus. Porro, perspiciatis ea neque debitis ipsum expedita
+          assumenda, esse cum obcaecati fugit nulla tenetur a repudiandae qui
+          enim amet sequi nesciunt dicta!
+        </p>
         <h2>About the Course</h2>
-        <p>Information about the course goes here...</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quidem
+          vitae atque ex quisquam velit adipisci minus sed nam sint mollitia
+          similique eos quasi maiores fugiat repellendus saepe eligendi earum
+          suscipit aliquid voluptas iste, eveniet voluptatem. Magnam eos vel sed
+          tempore, officia nam id ex dignissimos ipsa quidem nemo consequatur.
+        </p>
       </section>
     </>
   );
