b5f3151b281b9f8089dd1f0fcd2aec9cc547663e
Kotec <vivalastelmah@gmail.com>
2023-12-19 18:58:05 +0300

    feat: user can now select the base url. add server status indicator

--- src/components/EditorPanel/EditorPanel.tsx
+++ src/components/EditorPanel/EditorPanel.tsx
@@ -1,5 +1,11 @@
-import { updateUserQuery } from '../../features/projectSlice';
-import { useAppDispatch } from '../../hooks/appHooks';
+import { ChangeEvent, useState } from 'react';
+import { useSchemaQuery } from '../../features/apiSlice';
+import {
+  updateUserEndpoint,
+  updateUserQuery
+} from '../../features/projectSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/appHooks';
+import Loader from '../Loader/Loader';
 import classes from './EditorPanel.module.scss';
 
 export type EditorPanelProps = {
@@ -7,16 +13,62 @@ export type EditorPanelProps = {
 };
 
 export default function EditorPanel({ userQuery }: EditorPanelProps) {
+  const { userEndpoint } = useAppSelector((state) => state.project);
+  const [endpoint, setEndpoint] = useState(userEndpoint);
   const dispatch = useAppDispatch();
+
+  const { isError, isFetching } = useSchemaQuery(userEndpoint);
+
+  const updateEndpoint = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (endpoint !== userEndpoint && endpoint !== '') {
+      dispatch(updateUserEndpoint(endpoint.toString()));
+    }
+    if (endpoint === '') {
+      setEndpoint(userEndpoint);
+    }
+  };
+
   const submitQuery = async () => {
     dispatch(updateUserQuery(userQuery));
   };
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setEndpoint(value);
+  };
+
   return (
     <div className={classes.panel}>
-      <a
-        className={`${classes.editor_btn} ${classes.send_btn}`}
-        onClick={submitQuery}
-      />
+      <div className={classes.panel_items}>
+        <a
+          className={`${classes.editor_btn} ${classes.request_btn}`}
+          onClick={submitQuery}
+        />
+        <div className={classes.endpoint_container}>
+          <input
+            className={classes.endpoint_input}
+            placeholder="New GraphQL endpoint..."
+            onBlur={updateEndpoint}
+            onChange={onChangeHandler}
+            value={endpoint}
+            id="url"
+            name="url"
+            type="text"
+          />
+          {isFetching ? (
+            <Loader />
+          ) : isError ? (
+            <span
+              title="Server status"
+              className={`${classes.indicator} ${classes.indicator_error}`}
+            />
+          ) : (
+            <span className={`${classes.indicator} ${classes.indicator_ok}`} />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
