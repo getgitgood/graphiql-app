40cbc2ceba2f7caab61f8f3b7a339c638c816f33
alima987 <alima1996b@gmail.com>
2023-12-24 18:09:44 +0500

    feat: add localization to welcome page

--- src/components/Context/Context.tsx
+++ src/components/Context/Context.tsx
@@ -1,12 +1,19 @@
 import { createContext, useState, useContext } from 'react';
 import { ContextProps, LanguageContextProps, LanguageEnum } from '../../types';
+import enTranslations from '../../translations/en.json';
+import ruTranslations from '../../translations/ru.json';
 
-export const defaultValue = {
-  currentLanguage: LanguageEnum.EN,
-  changeLanguage: (value: string) => value,
-  LanguageEnum: LanguageEnum
+const translations = {
+  EN: enTranslations,
+  RU: ruTranslations
 };
 
+export const defaultValue: LanguageContextProps = {
+  currentLanguage: LanguageEnum.EN,
+  changeLanguage: (value: keyof typeof LanguageEnum) => value,
+  LanguageEnum,
+  translations: translations
+};
 export const AppContext = createContext<LanguageContextProps>(defaultValue);
 
 export default function AppContextProvider({ children }: ContextProps) {
@@ -22,6 +29,7 @@ export default function AppContextProvider({ children }: ContextProps) {
   };
 
   const values = {
+    translations,
     changeLanguage,
     currentLanguage,
     LanguageEnum
